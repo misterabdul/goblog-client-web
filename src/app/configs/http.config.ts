f17586f59e6f34d8f150e02f5a2b6762c7186da8
Abdul Rahmad Pasaribu <abdoelrachmad@gmail.com>
2022-03-08 13:20:06 +0700

    chore: define http options type

--- src/app/configs/http.config.ts
+++ src/app/configs/http.config.ts
@@ -1,7 +1,31 @@
-import { HttpHeaders } from '@angular/common/http';
+import { HttpContext, HttpHeaders, HttpParams } from '@angular/common/http';
+
+type HttpOptions = {
+  headers?:
+    | HttpHeaders
+    | {
+        [header: string]: string | string[];
+      };
+  context?: HttpContext;
+  observe?: 'body';
+  params?:
+    | HttpParams
+    | {
+        [param: string]:
+          | string
+          | number
+          | boolean
+          | ReadonlyArray<string | number | boolean>;
+      };
+  reportProgress?: boolean;
+  responseType?: 'json';
+  withCredentials?: boolean;
+};
 
 export default class HttpConfig {
-  public static getDefaultOptions(headers: HttpHeaders | null = null) {
+  public static getDefaultOptions(
+    headers: HttpHeaders | null = null
+  ): HttpOptions {
     let _headers = headers ?? new HttpHeaders();
     _headers = _headers.append('Accept', 'application/msgpack');
 
